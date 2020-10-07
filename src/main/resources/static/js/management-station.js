53a85728612387421ff1ba3f59717f629b2b40b5
tuankhac <tuankhac19594@gmail.com>
2020-10-07 10:40:32 +0700

    merge code

--- src/main/resources/static/js/management-station.js
+++ src/main/resources/static/js/management-station.js
@@ -16,7 +16,7 @@ var table = $('#tableDataView').DataTable({
     "columnDefs": [
         {
             "targets": '_all',
-            "render": $.fn.dataTable.render.text()
+            // "render": $.fn.dataTable.render.text()
         }
     ],
     "pagingType": "full_numbers",
@@ -24,7 +24,7 @@ var table = $('#tableDataView').DataTable({
         [10, 25, 50, -1],
         [10, 25, 50, "Tất cả"]
     ],
-    "lengthChange": false,
+    "lengthChange": true,
     "searching": true,
     "ordering": false,
     "info": true,
@@ -53,9 +53,9 @@ var table = $('#tableDataView').DataTable({
     ],
     "ajax": {
         headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbmlzdHJhdG9yIiwiYXV0aCI6IiIsImV4cCI6MTYwMTYyOTg3NH0.8ZUl8eDHqUUy_Xjp5QoxArJQoc0I6ABFH2r1amnrT_Csy0Kq1il0PfZW1A6t27aWYj7LkwPVWCPHm57YplTc9w'
+            'Authorization': token
         },
-        "url": "http://localhost:8080/api/v1/mail-config/get-list-mail-config-pagination",
+        "url": apiUrl + "mail-config/get-list-mail-config-pagination",
         "method": "POST",
         "contentType": "application/json",
         "data": function (d) {
@@ -71,8 +71,8 @@ var table = $('#tableDataView').DataTable({
             let responseJson = JSON.parse(response);
             let dataRes = {
                 "draw": draw,
-                "recordsFiltered": responseJson.recordFiltered,
-                "recordsTotal": responseJson.recordTotal,
+                "recordsFiltered": responseJson.recordsTotal,
+                "recordsTotal": responseJson.recordsTotal,
                 "data": []
             };
 
@@ -112,9 +112,10 @@ function createMailConfig(e) {
 
     $.ajax({
         headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbmlzdHJhdG9yIiwiYXV0aCI6IiIsImV4cCI6MTYwMTYyOTg3NH0.8ZUl8eDHqUUy_Xjp5QoxArJQoc0I6ABFH2r1amnrT_Csy0Kq1il0PfZW1A6t27aWYj7LkwPVWCPHm57YplTc9w'
+
+            'Authorization': token
         },
-        "url": "http://localhost:8080/api/v1/mail-config/create-mail-config",
+        "url": apiUrl + "mail-config/create-mail-config",
         "method": "POST",
         "contentType": "application/json",
         "data": JSON.stringify(data),
@@ -126,4 +127,168 @@ function createMailConfig(e) {
 
         }
     })
+}
+
+let objSearch = {
+    s_id: '',
+    s_ip: '',
+    s_port: '',
+    s_username: '',
+    s_password: '',
+    s_domain: '',
+    s_sendername: '',
+    s_email: '',
+    s_protocol: ''
+};
+
+$(document).ready(function () {
+    $('#tableDataView thead th').each(function () {
+        var title = $(this).text();
+        var dataId = $(this).attr("data-id");
+        if (dataId != null && dataId != undefined) {
+            $(this).html('<input id="'+ dataId +'" type="text" placeholder="Search ' + title + '" />');
+        }
+    });
+
+    // showLoading();
+    var draw = 0;
+    var table = $('#tableDataView').DataTable({
+        "columnDefs": [
+            {
+                "targets": '_all',
+                "render": $.fn.dataTable.render.text()
+            }
+        ],
+        "pagingType": "full_numbers",
+        "lengthMenu": [
+            [10, 25, 50, -1],
+            [10, 25, 50, "Tất cả"]
+        ],
+        "lengthChange": true,
+        "searching": false,
+        "ordering": false,
+        "info": true,
+        "autoWidth": false,
+        "scrollX": true,
+        "responsive": true,
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Nhập thông tin tìm kiếm",
+        },
+        "select": {
+            "style": "single"
+        },
+        "processing": true,
+        "serverSide": true,
+        "columns": [
+            {"data": "indexCount"},
+            {"data": "id"},
+            {"data": "ip"},
+            {"data": "port"},
+            {"data": "username"},
+            {"data": "password"},
+            {"data": "domain"},
+            {"data": "sender_name"},
+            {"data": "email"},
+            {"data": "protocol"}
+        ],
+        initComplete: function () {
+            // Apply the search
+            this.api().columns().every(function () {
+                var that = this;
+                $('input', this.header()).on('keyup change clear', function () {
+                    let id = $(this).attr("id");
+                    // if (that.search() !== this.value) {
+                    //
+                    // }
+                    objSearch[id] = this.value;
+                    that
+                        .search(JSON.stringify(objSearch))
+                        .draw();
+                });
+            });
+        },
+        "ajax": {
+            headers: {
+                'Authorization': token
+            },
+            "url": apiUrl + "mail-config/get-list-mail-config-pagination",
+            "method": "POST",
+            "contentType": "application/json",
+            "data": function (d) {
+                console.log(d);
+                draw = d.draw;
+                return JSON.stringify({
+                    "draw": d.draw,
+                    "start": Math.round(d.start / d.length),
+                    "length": d.length,
+                    "search": JSON.stringify(objSearch)
+                });
+            },
+            "dataFilter": function (response) {
+                objSearch = {
+                    s_id: '',
+                        s_ip: '',
+                        s_port: '',
+                        s_username: '',
+                        s_password: '',
+                        s_domain: '',
+                        s_sendername: '',
+                        s_email: '',
+                        s_protocol: ''
+                };
+                let responseJson = JSON.parse(response);
+                let dataRes = {
+                    "draw": draw,
+                    "recordsFiltered": responseJson.recordsTotal,
+                    "recordsTotal": responseJson.recordsTotal,
+                    "data": []
+                };
+
+                for (let i = 0; i < responseJson.content.length; i++) {
+                    dataRes.data.push({
+                        "indexCount": i + 1,
+                        "id": responseJson.content[i].id,
+                        "ip": responseJson.content[i].ip,
+                        "port": responseJson.content[i].port,
+                        "username": responseJson.content[i].username,
+                        "password": responseJson.content[i].password,
+                        "domain": responseJson.content[i].domain,
+                        "sender_name": responseJson.content[i].senderName,
+                        "email": responseJson.content[i].emailAddress,
+                        "protocol": responseJson.content[i].protocol
+                    })
+                }
+
+                return JSON.stringify(dataRes);
+            }
+        }
+    });
+});
+
+
+//set action for button control
+$('#btnCreate').on('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // set state for button control
+    $('#btnCreate').css('display', 'none');
+    $('#btnCopy').css('display', 'none');
+    $('#btnDelete').css('display', 'none');
+    $('#btnEdit').css('display', 'none');
+
+    // set state for button save and back
+    $('#btnSaveCreate').css('display', '');
+    $('#btnBackCreate').css('display', '');
+
+    // disable mouse click table
+    $('#wrap_table_data').css('pointer-events', 'none');
+
+    // clean form
+    formReset();
+});
+
+function formReset() {
+    $('#form_data')[0].reset();
 }
