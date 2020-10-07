000259f77c7370f6b76d096dfc37cd4e6e73a037
Hauvt <hauhanh1998@gmail.com>
2020-10-07 16:04:08 +0700

    commit html users_manage

--- src/main/java/com/neo/kttvwebadmin/controller/users_managers.java
+++ src/main/java/com/neo/kttvwebadmin/controller/users_managers.java
@@ -1,14 +1,17 @@
 package com.neo.kttvwebadmin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/users_manager")
 public class users_managers {
     @GetMapping
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        model.addAttribute("a", 1);
         return "/users_manager/users_manager";
     }
 }
