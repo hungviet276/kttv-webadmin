b06e626523cece0d76f439a726d47937da79c2a3
tuankhac <tuankhac19594@gmail.com>
2020-10-14 21:51:14 +0700

    update

--- src/main/java/com/neo/kttvwebadmin/controller/ManagementStationController.java
+++ src/main/java/com/neo/kttvwebadmin/controller/ManagementStationController.java
@@ -1,8 +1,11 @@
 package com.neo.kttvwebadmin.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 @Controller
 @RequestMapping("/management-station")
@@ -11,8 +14,26 @@ public class ManagementStationController {
     public String index() {
         return "management-station/index";
     }
-    @GetMapping("time-series/index.html")
-    public String configTimeSeries() {
-        return "management-station/time-series/index";
+
+    @ResponseBody
+    @PostMapping("check-connect")
+    public String checkConnect(@RequestParam String host, @RequestParam int port) {
+        String result = "";
+        try(Socket socketOfClient = new Socket(host, port);
+            BufferedReader is = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));) {
+            String responseLine;
+            while ((responseLine = is.readLine()) != null) {
+                System.out.println("Server: " + responseLine);
+                break;
+            }
+            result = "OK";
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + host);
+            result = "e.getMessage()";
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + host);
+            result = "e.getMessage()";
+        }
+        return result;
     }
 }
--- src/main/resources/static/js/management-station-time-series.js
+++ src/main/resources/static/js/management-station-time-series.js
@@ -58,7 +58,6 @@ var station =
                     console.log(data);
                     $("#stationType").select2({data: data});
                     $("#stationTypeId").select2({data: data});
-                    $("#stationTypeIdControl").select2({data: data});
                     global.disableLoading();
                 }
             });
@@ -745,91 +744,6 @@ var station =
         stationCodeKeyUp: function (event){
             let val = $("#stationCode").val().trim();
             $("#stationCode").val(val.toUpperCase());
-        },
-        preControl:function (index){
-            //lay thong tin cua row data dang tuong tac
-            let rowData = station.table.rows(index).data().toArray();
-            console.log(JSON.stringify(rowData));
-            $("#management-station-control").modal();
-            $("#stationTypeIdControl").val(rowData[0].stationTypeId).trigger('change');
-            $("#stationCodeControl").val(rowData[0].stationCode);
-            $("#stationNameControl").val(rowData[0].stationName);
-        },
-        control:function (){
-            let host = $("#hostControl").val().trim();
-            let port = $("#portControl").val().trim();
-            let command = $("#commandControl").val().trim();
-            let value = $("#valueInput").val().trim();
-            let description = $("#description").val().trim();
-
-            if(host.length < 1){
-                toastr.error('', 'Bạn chưa nhập host điều khiển');
-                $("#hostControl").focus();
-                return ;
-            }
-            if(port.length < 1){
-                toastr.error('', 'Bạn chưa nhập port điều khiển');
-                $("#portControl").focus();
-                return ;
-            }
-            global.showLoading();
-            $.ajax({
-                headers: {
-                    'Authorization': token
-                },
-                url: apiUrl + "management-station/check-connect",
-                method: "POST",
-                // contentType: "application/json",
-                data: {"host":host,"port":port},
-                success: function (data) {
-                    if (data == 'OK') {
-                        toastr.success('', 'Kết nối thành công');
-                    } else {
-                        toastr.error('Kết nối thất bại', data.message);
-                    }
-                    global.disableLoading();
-                },
-                error: function (err) {
-                    global.disableLoading();
-                    toastr.error("", "Lỗi thực hiện");
-                }
-            });
-        },
-        checkConnect:function (){
-            let host = $("#hostControl").val().trim();
-            let port = $("#portControl").val().trim();
-            if(host.length < 1){
-                toastr.error('', 'Bạn chưa nhập host điều khiển');
-                $("#hostControl").focus();
-                return ;
-            }
-            if(port.length < 1){
-                toastr.error('', 'Bạn chưa nhập port điều khiển');
-                $("#portControl").focus();
-                return ;
-            }
-            global.showLoading();
-            $.ajax({
-                headers: {
-                    'Authorization': token
-                },
-                url: "/management-station/check-connect",
-                method: "POST",
-                // contentType: "application/json",
-                data: {"host":host,"port":port},
-                success: function (data) {
-                    if (data == 'OK') {
-                        toastr.success('', 'Kết nối thành công');
-                    } else {
-                        toastr.error('Kết nối thất bại', data.message);
-                    }
-                    global.disableLoading();
-                },
-                error: function (err) {
-                    global.disableLoading();
-                    toastr.error("", "Lỗi thực hiện");
-                }
-            });
         }
     }
 
@@ -862,7 +776,7 @@ $(document).ready(function () {
             className: 'select-checkbox',
             targets: 0
         }, {
-            targets: 14,
+            targets: 13,
             render: function (data, type, row) {
                 if (data === 1) {
                     return '<div class="status_green">Hoạt động</div>';
@@ -903,7 +817,6 @@ $(document).ready(function () {
         "columns": [
             {"data": ""},
             {"data": "indexCount"},
-            {"data": "control"},
             {"data": "objectType"},
             {"data": "objectTypeName"},
             {"data": "stationCode"},
@@ -992,7 +905,6 @@ $(document).ready(function () {
                     dataRes.data.push({
                         "": "",
                         "indexCount": i + 1,
-                        "control":"<span class='fa fa-wrench' title='Điều khiển'  onclick='station.preControl("+i+")' style='cursor: pointer'></span>",
                         "stationLongName": responseJson.content[i].stationLongName,
                         "objectType": responseJson.content[i].objectType,
                         "objectTypeName": responseJson.content[i].objectTypeName,
@@ -1083,6 +995,4 @@ $(document).ready(function () {
         station.table.row(station.indexOfRow).deselect();
         station.show_search();
     });
-
-    $("#commandControl").prop('disabled',false);
 });
