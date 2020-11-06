5c273d1f9ff54c95aec698ef24d5c1409474f019
acer <levietthang1997@gmail.com>
2020-11-06 14:48:29 +0700

    - update:  fix bugs

---
+++ src/main/java/com/neo/kttvwebadmin/controller/DemoArcgisMapController.java
@@ -0,0 +1,26 @@
+package com.neo.kttvwebadmin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author thanglv on 11/5/2020
+ * @project NBD
+ */
+
+@Controller
+@RequestMapping("/demo-arcgis")
+public class DemoArcgisMapController {
+
+    @GetMapping("/station")
+    public String displayStationMap() {
+        return "demo/demo_arcgis_station";
+    }
+
+    @GetMapping("/station-embedded")
+    public String stationEmbedded() {
+        return "demo/station_embedded";
+    }
+}
