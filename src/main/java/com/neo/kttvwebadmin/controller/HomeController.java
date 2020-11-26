6e7f3d0bacebc0f806ef54e2157c9f1c4676fbe6
acer <levietthang1997@gmail.com>
2020-11-26 14:31:01 +0700

    fix chart in map

--- src/main/java/com/neo/kttvwebadmin/controller/HomeController.java
+++ src/main/java/com/neo/kttvwebadmin/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
+
     @GetMapping
     public String getHomePage() {
         return "index";
